e4527cd9b9de05dd4105dd44556a60db8b3f3ada
Alejandro Becerra Acevedo <131910652+AlejandroBecerraAcevedo@users.noreply.github.com>
2025-06-28 17:00:22 -0500

    Update Data.scala

--- src/test/scala/Demo/Data.scala
+++ src/test/scala/Demo/Data.scala
@@ -1,7 +1,7 @@
 package Demo
 
 object Data{
-    val url= "https://thinking-tester-contact-list.herokuapp.com/users"
+    val url= "https://thinking-tester-contact-list.herokuapp.com/"
     val email = "alejo@example.com"
     val password = "alejandrobecerra"
 }
